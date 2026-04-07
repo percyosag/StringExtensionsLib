2839a9b243ff9b72587d43c5aa4da51f43c4f2e2
Percy Osunde <posunde@my.centennialcollege.ca>
2026-04-06 22:28:48 -0400

    Add project files.

---
+++ StringExtensionsLib/StringExtensions.cs.cs
@@ -0,0 +1,13 @@
+﻿namespace StringExtensionsLib
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpper(this string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return false;
+
+            return char.IsUpper(value[0]);
+        }
+    }
+}
